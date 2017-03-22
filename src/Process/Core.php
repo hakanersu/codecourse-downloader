f4211c8ee7e29c494b5f5d148de06029f8c3e37b
Hakan ERSU <hakanersu@gmail.com>
2017-03-23 00:57:51 +0300

    Progress bar update

--- src/Process/Core.php
+++ src/Process/Core.php
@@ -44,7 +44,7 @@ class Core
                 echo PHP_EOL;
                 $this->output->writeln("<info>Downloading series: {$lesson}</info>");
                 $progress = new ProgressBar($this->output, $remoteLessons->count());
-                $progress->setFormat('very_verbose');
+                $progress->setFormat(' %current%/%max% [%bar%] %percent:3s%% %elapsed:6s%/%estimated:-6s% %memory:6s%');
                 $progress->start();
                 foreach ($remoteLessons as $item) {
                     if (!$files->file->has($lesson.'/'.$item['slug'])) {
