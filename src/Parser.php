9e787ad82824d616ed15ff6319920dfb2091c22a
Hakan ERSU <hakanersu@gmail.com>
2019-11-29 17:09:57 +0300

    test

--- src/Parser.php
+++ src/Parser.php
@@ -40,45 +40,44 @@ class Parser
                 //throw new \Exception("Can't parse lesson page, page structure changed.");
                 return;
             }
-            $re = '/parts:({parts:(\[(.*)\])}?),paths/m';
+            $re = '/parts:{parts:([\s\S]*),paths/m';
             preg_match($re, $text, $matches, PREG_OFFSET_CAPTURE, 3);
-            $parts = $matches[3][0];
+
+            $parts = $matches[0][0];
 
             $re = '/id:(.*?),title:"(.*?)",slug:(.*?),(.*?)order:(.*?),(.*?)video:{data:{id:(.*?),/m';
             preg_match_all($re, $parts, $matches, PREG_SET_ORDER, 0);
             $info = [];
             $i = 1;
 
-           $this->extractFunctionValues($text);
+            $this->extractFunctionValues($text);
 
             foreach ($matches as $match) {
-                if (strlen($match[3]) <=3) {
+                if (strlen($match[3]) <= 3) {
                     $match[3] = strtolower(trim(preg_replace('/[^A-Za-z0-9-]+/', '-', $match[2])));
                 }
                 $test = false;
-                if ($test && $match[2] == "Fixing up failing order test") {
-                    dd($match,[
-                        'order' => is_string($match[5]) ? $i : $match[5] ,
+                if ($test && $match[2] == 'Fixing up failing order test') {
+                    dd($match, [
+                        'order' => is_string($match[5]) ? $i : $match[5],
                         'title' => $match[2],
                         'slug' => str_replace('"', '', $match[3]),
-                        'video' => is_numeric($match[7]) ? $match[7] : $this->getJsValue($match[7])
+                        'video' => is_numeric($match[7]) ? $match[7] : $this->getJsValue($match[7]),
                     ]);
                 }
                 array_push($info, [
-                    'order' => is_string($match[5]) ? $i : $match[5] ,
+                    'order' => is_string($match[5]) ? $i : $match[5],
                     'title' => $match[2],
                     'slug' => str_replace('"', '', $match[3]),
-                    'video' => is_numeric($match[7]) ? $match[7] : $this->getJsValue($match[7])
+                    'video' => is_numeric($match[7]) ? $match[7] : $this->getJsValue($match[7]),
                 ]);
-                $i ++;
+                ++$i;
             }
             $lessons = $this->lesson($info);
         });
 
         return $lessons;
     }
-
-
 
     public function extractFunctionValues($text)
     {
@@ -87,15 +86,16 @@ class Parser
         $parameters = explode(',', $matches[0][1]);
         $values = $this->extractValues($text);
 
-        $this->jsfunction =  (object) [
+        $this->jsfunction = (object) [
             'parameters' => $parameters,
-            'values' => $values
+            'values' => $values,
         ];
     }
 
     public function getJsValue($param)
     {
-        $index = array_search($param, $this->jsfunction->parameters);
+        $index = array_search($param, $this->jsfunction->parameters, true);
+
         return $this->jsfunction->values[$index];
     }
 
@@ -103,7 +103,8 @@ class Parser
     {
         $re = '/serverRendered:(.*)}}(.*?)\((.*?)\)/m';
         preg_match_all($re, $text, $matches, PREG_SET_ORDER, 0);
-        return  str_getcsv($matches[0][3], ",", '"');
+
+        return  str_getcsv($matches[0][3], ',', '"');
     }
 
     public function lesson($nuxt)
@@ -114,10 +115,11 @@ class Parser
                 'link' => getenv('API') . '/api/videos/' . $part['video'] . '/download?quality=hd',
                 'title' => $part['title'],
                 'slug' => $part['slug'],
-                'filename' => sprintf('%02d', (int)$part['order']-1) . '-' . $part['slug'] . '.mp4',
+                'filename' => sprintf('%02d', (int) $part['order'] - 1) . '-' . $part['slug'] . '.mp4',
             ];
             $lessons[] = $lesson;
         }
+
         return $lessons;
     }
 }
